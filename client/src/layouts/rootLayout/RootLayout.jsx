a489386456fb910f6630ba6dcf08db5ddb006896
ksmanoj3221 <ksmanoj3221@gmail.com>
2024-09-09 20:01:17 +0530

    QueryClientProvider

--- client/src/layouts/rootLayout/RootLayout.jsx
+++ client/src/layouts/rootLayout/RootLayout.jsx
@@ -8,6 +8,7 @@ import {
   SignInButton,
   UserButton,
 } from "@clerk/clerk-react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Import your publishable key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -16,6 +17,9 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
+// Create a client
+const queryClient = new QueryClient();
+
 const RootLayout = () => {
   return (
     <ClerkProvider
@@ -23,22 +27,24 @@ const RootLayout = () => {
       afterSignOutUrl="/"
       // signInUrl="/sign-in"
     >
-      <div className="rootLayout">
-        <header>
-          <Link to="/" className="logo">
-            <img src="/logo.png" alt="" />
-            <span>Inquiro BOT</span>
-          </Link>
-          <div className="user">
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </div>
-        </header>
-        <main>
-          <Outlet />
-        </main>
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div className="rootLayout">
+          <header>
+            <Link to="/" className="logo">
+              <img src="/logo.png" alt="" />
+              <span>Inquiro BOT</span>
+            </Link>
+            <div className="user">
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
+          </header>
+          <main>
+            <Outlet />
+          </main>
+        </div>
+      </QueryClientProvider>
     </ClerkProvider>
   );
 };
